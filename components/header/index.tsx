427ea877dbf0fd37e529e415244acd92758e19d2
bhagyasah <bhagyasah4u@gmail.com>
2021-12-04 15:29:29 +0545

    setup completed

---
+++ components/header/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { title } from './style'
+
+export default function index() {
+  return (
+    <div>
+      <h1 css={title}>this is header component</h1>
+    </div>
+  )
+}
---
+++ components/header/style.ts
@@ -0,0 +1,6 @@
+import { css } from '@emotion/react'
+
+const title = css`
+  color: red;
+`
+export { title }
