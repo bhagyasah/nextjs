f96328490f8a9a7e703a9e5f2ee3c3e8afc5dcf6
bhagyasah <bhagyasah4u@gmail.com>
2021-12-05 17:55:26 +0545

    blog post part start

---
+++ components/footer/index.tsx
@@ -0,0 +1,55 @@
+import { IoLogoGithub as Github } from 'react-icons/io'
+import { FiTwitter as Twitter } from 'react-icons/fi'
+import { SiUdemy as Udemy, SiRss as Rss } from 'react-icons/si'
+import { metadata } from 'config'
+import { leakedList } from '@shared/style-helpers'
+import Footprint from '@components/footprint'
+import { footer } from './styles'
+import Link from 'next/link'
+
+const Footer = () => (
+  <footer css={footer}>
+    <ul css={leakedList}>
+      <li>
+        <a
+          aria-label="To Atila Fassina's github profile"
+          href={metadata.author.githubUrl}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Github />
+        </a>
+      </li>
+      <li>
+        <a
+          aria-label="To Atila Fassina's twitter profile"
+          href={metadata.author.twitterUrl}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Twitter />
+        </a>
+      </li>
+      <li>
+        <a
+          aria-label="To this course's Udemy page"
+          href={metadata.author.udemyUrl}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Udemy />
+        </a>
+      </li>
+      <li>
+        <Link href="/api/rss">
+          <a aria-label="Get RSS feed">
+            <Rss />
+          </a>
+        </Link>
+      </li>
+    </ul>
+    <Footprint />
+  </footer>
+)
+
+export default Footer
---
+++ shared/get-posts.ts
@@ -0,0 +1,48 @@
+import { promises as fs, readdirSync } from 'fs'
+import path from 'path'
+import renderToString from 'next-mdx-remote/render-to-string'
+import matter from 'gray-matter'
+import mdxComponents from '@shared/mdx-components'
+import type { PostFile, FrontMatter } from './types'
+
+const getDirData = (source: string): PostFile[] =>
+  readdirSync(source).map((name) => ({
+    filepath: `${source}/${name}`,
+    slug: name.replace(new RegExp(path.extname(name) + '$'), ''),
+  }))
+
+const formatPostList = async ({ filepath, slug }: PostFile) => {
+  const mdxSource = await fs.readFile(filepath)
+  const { content, data } = matter(mdxSource)
+  const frontMatter = data as FrontMatter
+
+  const mdx = await renderToString(content, {
+    components: mdxComponents,
+    scope: frontMatter,
+  })
+
+  return {
+    filepath,
+    slug,
+    content,
+    frontMatter,
+    mdx,
+  }
+}
+
+export async function getPosts(source: string) {
+  const files = getDirData(source)
+
+  if (files.length === 0) return []
+
+  const content = await Promise.all(files.map(formatPostList))
+
+  content.sort((post1, post2) => {
+    const dateMs1 = +new Date(post1.frontMatter.date)
+    const dateMs2 = +new Date(post2.frontMatter.date)
+
+    return dateMs2 - dateMs1
+  })
+
+  return content
+}
